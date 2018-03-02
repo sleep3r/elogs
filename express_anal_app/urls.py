c22ee5456e105b365e0dd8f3b94779093fe96e9f
Kirill Khudyakov <hudiakov@gmail.com>
2018-03-02 17:34:04 +0300

    Add ‘jea/edit’ to urls

--- express_anal_app/urls.py
+++ express_anal_app/urls.py
@@ -4,9 +4,10 @@ from express_anal_app import views
 
 urlpatterns = [
     url('^$', views.index),
+    url('jea/edit$', views.leaching_jea_edit),
     url('jea', views.leaching_jea),
     url('ju', views.leaching_ju),
     url('jrk', views.leaching_jrk),
     url('main', views.electrolysis),
-    url('edit', views.electrolysisEdit)
+    url('edit$', views.electrolysisEdit)
 ]
