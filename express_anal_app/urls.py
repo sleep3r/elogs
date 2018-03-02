272791e42d5de6265114d3bcb5f737e4f403b535
Dmitriy Yampolskiy <yampolskiydv@gmail.com>
2018-03-02 14:59:10 +0300

    Put away the $ sign.

--- express_anal_app/urls.py
+++ express_anal_app/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, include
 from express_anal_app import views
 
 urlpatterns = [
-    url('^$', views.index),
+    url('^', views.index),
     url('jea', views.leaching_jea),
     url('ju', views.leaching_ju),
     url('jrk', views.leaching_jrk),
