5ba12bbb94cbb4349d05446ff45eaf9fc882d05b
Kirill Khudyakov <hudiakov@gmail.com>
2018-03-16 13:26:27 +0300

    Add save/tanks to urls

--- express_anal_app/urls.py
+++ express_anal_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url('^$', views.index),
     url('jea/edit$', views.leaching_jea_edit),
     url('all/edit$', views.leaching_all_edit),
+    url('save/tanks$', views.leaching_save_tanks),
     url('jea', views.leaching_jea),
     url('ju', views.leaching_ju),
     url('jrk', views.leaching_jrk),
