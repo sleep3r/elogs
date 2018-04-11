3c6f15d97e88c5e85498cc6d06004c328ce2c9e6
Victor Golovnin <victor.golovnin@gmail.com>
2018-04-11 17:33:39 +0300

    Import furnace data from backend

--- assets/App.vue
+++ assets/App.vue
@@ -1,10 +1,24 @@
 <template>
-  <div> HELLO FROM VUE WEBPACK</div>
+  <div> FURNACE
+    <span v-if="furnace_data['1']"> {{ furnace_data }} </span>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'app'
+  name: 'app',
+  data() { 
+    return {
+      furnace_data: {}
+    }
+  },
+  created() {
+    axios.get('/furnace/frac/measurements/get').then(({ data }) => {
+      this.furnace_data = data.data
+    }) 
+  }
 }
 </script>
 
