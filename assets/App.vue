c43e71c41c1440af442b2a788531133c24df1936
Victor Golovnin <victor.golovnin@gmail.com>
2018-04-13 16:39:44 +0300

    Add furnace time selectors

--- assets/App.vue
+++ assets/App.vue
@@ -1,17 +1,55 @@
 <template>
   <div> FURNACE
-    <span v-if="furnace_data['1']"> {{ furnace_data }} </span>
+    <div class="x_panel">
+      <div class="x_title">Cinder Date</div>
+      <div class="x_content">
+        <select name="center_data" class="form-control" v-model="cinder_index">
+          <option v-for="(cinder, index) in cinder_data" :key="cinder.time" :value="index">{{ cinder.time }}</option>
+        </select>
+      </div>
+      {{ cinder_current }}
+    </div>
+    <div class="x_panel">
+      <div class="x_title">Schieht Date</div>
+      <div class="x_content">
+        <select name="center_data" class="form-control" v-model="schieht_index">
+          <option v-for="(schieht, index) in cinder_data" :key="schieht.time" :value="index">{{ schieht.time }}</option>
+        </select>
+      </div>
+      {{ schieht_current }}
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import _ from 'lodash'
 
 export default {
   name: 'app',
   data() { 
     return {
-      furnace_data: {}
+      furnace_data: {},
+      cinder_index: -1,
+      schieht_index: -1
+    }
+  },
+  computed: {
+    cinder_data() {
+      return _.transform(this.furnace_data, (r, v, k) => {
+        r.push(v.cinder)
+      }, [])
+    },
+    cinder_current() {
+      return this.cinder_data[this.cinder_index]
+    },
+    schieht_data() {
+      return _.transform(this.furnace_data, (r, v, k) => {
+        r.push(v.schieht)
+      }, [])
+    },
+    schieht_current() {
+      return this.schieht_data[this.schieht_index]
     }
   },
   created() {
