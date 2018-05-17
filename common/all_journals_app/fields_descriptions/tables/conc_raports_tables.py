c21e18c4645b1eeeb073cf32dd03f9e9231fbca8
dimitrius <kolbasa12393@mail.ru>
2018-05-17 04:14:06 +0300

    Decreased readability for the sake of the code size

--- common/all_journals_app/fields_descriptions/tables/conc_raports_tables.py
+++ common/all_journals_app/fields_descriptions/tables/conc_raports_tables.py
@@ -1,22 +1,22 @@
 import json
 from utils.deep_dict import deep_dict
 
-big_table_desc = deep_dict()
+tt = deep_dict()
 
 # arrays
-big_table_desc.wagon_num = dict(type="number", min_normal=0, max_normal=100)
-big_table_desc.conc_num = dict(type="text", min_normal=0, max_normal=100)
+tt.wagon_num = dict(type="number", min_normal=0, max_normal=100)
+tt.conc_num = dict(type="text", min_normal=0, max_normal=100)
 
-big_table_desc.supply_time = dict(type="time", min_normal=0, max_normal=100)
-big_table_desc.dispatch_time = dict(type="time", min_normal=0, max_normal=100)
-big_table_desc.downtime = dict(type="time", min_normal=0, max_normal=100)
+tt.supply_time = dict(type="time", min_normal=0, max_normal=100)
+tt.dispatch_time = dict(type="time", min_normal=0, max_normal=100)
+tt.downtime = dict(type="time", min_normal=0, max_normal=100)
 
-big_table_desc.recieved_conc = dict(type="number", min_normal=5, max_normal=100)
-big_table_desc.recieved_beds = dict(type="number", min_normal=0, max_normal=100)
-big_table_desc.recieved_stops = dict(type="number", min_normal=0, max_normal=100)
-big_table_desc.recieved_braces = dict(type="number", min_normal=0, max_normal=100)
+tt.recieved_conc = dict(type="number", min_normal=5, max_normal=100)
+tt.recieved_beds = dict(type="number", min_normal=0, max_normal=100)
+tt.recieved_stops = dict(type="number", min_normal=0, max_normal=100)
+tt.recieved_braces = dict(type="number", min_normal=0, max_normal=100)
 
-big_table_desc = big_table_desc.clear_empty().get_dict()
+big_table_desc = tt.clear_empty().get_dict()
 
 # --------------------------------------- small table -----------------------------------------------------
 
