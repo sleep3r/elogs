b72c17726c54cbf710cdcf8e467315ae76bf2084
NikitaKrutoy <nikitakrutoy@gmail.com>
2018-05-21 15:56:19 +0300

    temp urls for electrolysis

--- DigitalLogs/urls.py
+++ DigitalLogs/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     url('leaching', include('leaching.express_anal_app.urls')),
     url('^furnace/fractional', include('furnace.fractional_app.urls')),
     url('furnace/concentrate', include('furnace.concentrate_report_app.urls')),
+    url('electrolysis/', include('furnace.concentrate_report_app.urls')),
     url('^auth', include('login_app.urls')),
     url('^common', include('common.all_journals_app.urls')),
 ]
