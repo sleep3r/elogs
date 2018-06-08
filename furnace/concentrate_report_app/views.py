5e58355866805398386a8cbaf701eed57f7c1b04
dimitrius <kolbasa12393@mail.ru>
2018-06-08 22:04:41 +0300

    Some changes

--- furnace/concentrate_report_app/views.py
+++ furnace/concentrate_report_app/views.py
@@ -25,6 +25,10 @@ def tpp(request):
     upper_table = deep_dict()
     lower_table = deep_dict()
 
+    control_table = deep_dict()
+    control_table.title = 'Управление всем'
+    control_table.name = 'all_journals_app/journal_control_panel.html'
+
     big_table.title = "Большая таблица"
     big_table.name = 'concentrate_report_tables/big_table.html'
 
@@ -37,8 +41,7 @@ def tpp(request):
     lower_table.title = "Нижняя таблица"
     lower_table.name = 'concentrate_report_tables/lower_table.html'
 
-    context.tables = [upper_table, big_table, small_table, lower_table]
-    # context.tables = [big_table, small_table]
+    context.tables = [control_table, upper_table, big_table, small_table, lower_table]
 
     template = loader.get_template('common.html')
     return HttpResponse(template.render(context.get_dict(), request))
