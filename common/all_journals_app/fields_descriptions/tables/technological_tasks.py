8d1c6e2e3447634fa70d8d57b006a9b177df76eb
sleep3r <kalashnikov@os-evolution.ru>
2018-06-26 12:27:53 +0300

    Made adjustments in the journal of technical assignments

--- common/all_journals_app/fields_descriptions/tables/technological_tasks.py
+++ common/all_journals_app/fields_descriptions/tables/technological_tasks.py
@@ -4,8 +4,20 @@ from utils.deep_dict import deep_dict
 
 
 ts = deep_dict()
-ts.date_create = dict(type="date")
+ts.date_create = date_default
 ts.comments = text_default
-ts.employee = text_default
+ts.caption = dict(type="datalist", min_normal=10, max_normal=20000, options=["ЗГОК", "Арт-ий",
+                                                                             "Усть-ТАЛ",
+                                                                             "Карагайлы",
+                                                                             "Верх-Бер",
+                                                                             "Белоусовка",
+                                                                             "Жезкент",
+                                                                             "Н.Широкинский",
+                                                                             "Лесосиб",
+                                                                             "Алтын-Топкан"])
+
+ts.weight = ton_default
+
+ts.employee = employee_default
 
 technologial_tasks_main_desc = ts.clear_empty().get_dict()
