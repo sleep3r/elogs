80293157d39513e688953484b586aec660c188d6
dimitrius <kolbasa12393@mail.ru>
2018-06-29 19:56:25 +0300

    unique together

--- common/all_journals_app/models.py
+++ common/all_journals_app/models.py
@@ -16,6 +16,9 @@ class CellValue(models.Model):
 
     value = models.CharField(max_length=1024, verbose_name='Значение поля')
 
+    class Meta:
+        unique_together = ('journal_page', 'table_name', 'field_name', 'index')
+
     def __str__(self):
         return "journal_page: " + str(self.journal_page) + " table_name: " \
                + str(self.table_name) + " field_name: " + str(self.field_name) +\
