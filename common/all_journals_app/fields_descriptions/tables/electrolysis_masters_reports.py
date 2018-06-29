233fa8e39eee5f1adaa3b39f10acbbfc8ea8fe4f
sleep3r <kalashnikov@os-evolution.ru>
2018-06-29 19:11:31 +0300

    Issue #18

--- common/all_journals_app/fields_descriptions/tables/electrolysis_masters_reports.py
+++ common/all_journals_app/fields_descriptions/tables/electrolysis_masters_reports.py
@@ -25,25 +25,42 @@ s4 = deep_dict()
 
 s1.h2so4_fact = numeric_default
 s1.zn_fact = numeric_default
-s1.sm_fact = numeric_default
+s1.t_sm_fact = temperature_default
 s1.q_fact = numeric_default
 s1.h2so4_nesootv = numeric_default
 s1.zn_nesootv = numeric_default
-s1.sm_nesootv = numeric_default
+s1.t_sm_nesootv = temperature_default
 s1.q_nesootv = numeric_default
 s1.h2so4_mery = text_default
 s1.zn_mery = text_default
-s1.sm_mery = text_default
+s1.t_sm_mery = temperature_default
 s1.q_mery = text_default
 s1.h2so4_rosp = text_default
 s1.zn_rosp = text_default
-s1.sm_rosp = text_default
+s1.t_sm_rosp = temperature_default
 s1.q_rosp = text_default
 s1.gradirni = text_default
+s1.cooler1 = number_default #
+s1.cooler2 = number_default
+s1.cooler3 = number_default
+s1.cooler4 = number_default
+s1.cooler5 = number_default
+s1.cooler6 = number_default
+s1.cooler7 = number_default
+s1.cooler8 = number_default
+s1.t_otr_fact = temperature_default
+s1.t_otr_nesootv = temperature_default
+s1.t_otr_mery = temperature_default
+s1.t_otr_rosp = temperature_default
 
 
 pt = deep_dict()
-pt.percent_Pb = numeric_default
+pt.percent_Pb = percent_default
+pt.percent_Cu = percent_default
+pt.percent_Fe = percent_default
+pt.percent_Cd = percent_default
+pt.percent_Al = percent_default
+pt.percent_Zn = percent_default
 # pt.n_Pb = text_default
 # pt.mery_Pb = text_default
 
