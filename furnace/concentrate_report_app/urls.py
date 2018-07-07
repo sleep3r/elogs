1c206e524a633a02115242e95d11a75559d2a482
Kirill Khudyakov <hudiakov@gmail.com>
2018-07-07 16:07:18 +0300

    Fix url to concentrate_report_journal

--- furnace/concentrate_report_app/urls.py
+++ furnace/concentrate_report_app/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import url, include
 from django.urls import path
 
 urlpatterns = [
-    url('tpp$', views.tpp),
-    url('^$', views.index)
+    url('^/$', views.index),
 ]
--- furnace/concentrate_report_app/views.py
+++ furnace/concentrate_report_app/views.py
@@ -12,9 +12,26 @@ from common.all_journals_app.services.context_creator import get_common_context
 
 @login_required
 def index(request):
-    context = {}
+    context = get_common_context(journal_name="concentrate_report_journal", request=request)
+    context.journal_title = 'Журнал рапортов о проделанной работе по складам концентратов';
+
+    big_table = deep_dict()
+    small_table = deep_dict()
+    upper_table = deep_dict()
+    lower_table = deep_dict()
+
+    big_table.name = 'concentrate_report_tables/big_table.html'
+
+    small_table.name = 'concentrate_report_tables/small_table.html'
+
+    upper_table.name = 'concentrate_report_tables/upper_table.html'
+
+    lower_table.name = 'concentrate_report_tables/lower_table.html'
+
+    context.tables = [upper_table, big_table, small_table, lower_table]
+
     template = loader.get_template('common.html')
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render(context.get_dict(), request))
 
 
 @login_required
