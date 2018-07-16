b82755ec9a655b1fe565272a24386ea48784a9e6
Kirill Khudyakov <hudiakov@gmail.com>
2018-07-16 10:47:12 +0300

    Some improve import_users

--- common/all_journals_app/management/commands/import_users.py
+++ common/all_journals_app/management/commands/import_users.py
@@ -2,11 +2,12 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 import csv
 
+from utils.usersutils import add_user
 from utils.webutils import translate
 
 
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
+    help = 'Adding users from csv file'
 
     # def add_arguments(self, parser):
     #     parser.add_argument(
@@ -20,28 +21,20 @@ class Command(BaseCommand):
             users_info = csv.reader(csvfile, delimiter=';', quotechar='|')
             for row in users_info:
                 user_fio = row[0].split(",", 1)[0]
-                fio_ru_tuple = user_fio.split()
-                translated_user_name = translate(user_fio)
-                translated_user_name_tuple = translated_user_name.split("-")
-                user_dict = {
+                user_ru = user_fio.split()
+                user_en = translate(user_fio).split("-")
+                user = {
                     'ru': {
-                        'last_name': fio_ru_tuple[0],
-                        'first_name': fio_ru_tuple[1] if 1 < len(fio_ru_tuple) else '',
-                        'second_name': fio_ru_tuple[2] if 2 < len(fio_ru_tuple) else '',
+                        'last_name': user_ru[0],
+                        'first_name': user_ru[1] if 1 < len(user_ru) else '',
+                        'second_name': user_ru[2] if 2 < len(user_ru) else '',
                     },
                     'en': {
-                        'last_name': translated_user_name_tuple[0],
-                        'first_name': translated_user_name_tuple[1] if 1 < len(translated_user_name_tuple) else '',
-                        'second_name': translated_user_name_tuple[2] if 2 < len(translated_user_name_tuple) else ''
+                        'last_name': user_en[0],
+                        'first_name': user_en[1] if 1 < len(user_en) else '',
+                        'second_name': user_en[2] if 2 < len(user_en) else ''
                     }
                 }
-                user_name = user_dict['en']['last_name'] \
-                            + "-" + user_dict['en']['first_name'] \
-                            + "-" + user_dict['en']['second_name']
-                user = User.objects.create_user(user_name, password='qwerty')
-                user.first_name = user_dict['ru']['first_name']
-                user.last_name = user_dict['ru']['last_name']
-                user.is_superuser = False
-                user.is_staff = True
-                user.save()
 
+                add_user(user)
+        print("Users added")
