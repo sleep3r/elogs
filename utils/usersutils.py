e52b7d605519e3c717c6099709c582cbc97787c5
Kirill Khudyakov <hudiakov@gmail.com>
2018-07-16 10:44:33 +0300

    Added handle if user added before

--- utils/usersutils.py
+++ utils/usersutils.py
@@ -4,12 +4,12 @@ def add_user(user_dict):
     user_name = user_dict['en']['last_name'] \
                 + "-" + user_dict['en']['first_name'] \
                 + "-" + user_dict['en']['second_name']
-    user = User.objects.create_user(user_name, password='qwerty')
-    user.first_name = user_dict['ru']['first_name']
-    user.last_name = user_dict['ru']['last_name']
-    user.is_superuser = False
-    user.is_staff = True
-    user.save()
-
-
-
+    if User.objects.filter(username=user_name).exists():
+        print(f'user `{user_name}` already exists')
+    else:
+        user = User.objects.create_user(user_name, password='qwerty')
+        user.first_name = user_dict['ru']['first_name']
+        user.last_name = user_dict['ru']['last_name']
+        user.is_superuser = False
+        user.is_staff = True
+        user.save()
