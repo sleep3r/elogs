89947bfb7ab488a01854fa6cd9522851271c6ebe
“Alexander <kalashnikov@os-evolution.ru>
2018-07-21 16:26:37 +0300

    Removed loggers.py

--- e_logs/core/middleware.py
+++ e_logs/core/middleware.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-
+import logging
+import sys
 
 class ExceptionMiddleware(object):
     def __init__(self, get_response):
@@ -10,3 +11,23 @@ class ExceptionMiddleware(object):
 
     def process_exception(self, request, exception):
         pass
+
+class StdLogger(object):
+    def __init__(self, logger, log_level=logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
+
+    def flush(self):
+        pass
+
+
+stdout_logger = StdLogger(logging.getLogger('STDOUT'), logging.INFO)
+sys.stdout = stdout_logger
+
+stderr_logger = StdLogger(logging.getLogger('STDERR'), logging.ERROR)
+sys.stderr = stderr_logger
