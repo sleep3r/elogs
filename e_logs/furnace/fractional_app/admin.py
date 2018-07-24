2a4ab7b43f4ff3b4a19a25383bc74875b1548c95
“Alexander <kalashnikov@os-evolution.ru>
2018-07-24 23:07:23 +0300

    Fractional app on CellValue and JournalPage

--- e_logs/furnace/fractional_app/admin.py
+++ e_logs/furnace/fractional_app/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
-from e_logs.furnace.fractional_app.models import *
 
 # Register your models here.
 
-admin.site.register(Measurement)
-admin.site.register(SchiehtMeasurement)
-admin.site.register(Weight)
-admin.site.register(MeasurementPair)
