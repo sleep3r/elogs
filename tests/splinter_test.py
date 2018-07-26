dacc832dbd2fa45abde81bdda54b31d8b88556a5
Kirill Khudyakov <hudiakov@gmail.com>
2018-07-26 20:40:39 +0300

    Improve splinter_test
    express_analysis

--- tests/splinter_test.py
+++ tests/splinter_test.py
@@ -2,14 +2,13 @@ from splinter import Browser
 
 
 browser = Browser("chrome")
-browser.visit("http://127.0.0.1:8000")
-browser.fill("username", "inframine")
-browser.fill("password", "Singapore2017")
-browser.find_by_css("input.btn").click()
-# browser.fill_by_id("login-username")
-# browser.find_by_css(".timer_add .btn").click()
-
+browser.visit("http://127.0.0.1:8000/leaching/leaching_express_analysis")
 if browser.is_element_present_by_id("loginform", 2):
+    browser.fill("username", "inframine")
+    browser.fill("password", "Singapore2017")
+    browser.find_by_css("input.btn").click()
+
+if browser.is_element_present_by_id("table_id_loads_table", 2):
     browser.driver.save_screenshot("screenshot.png")
     print("All OK")
 else:
