5c056ef35bb7a6b76fa5fb1e43e7be74b5b578ea
“Alexander <kalashnikov@os-evolution.ru>
2018-07-30 21:31:46 +0300

    Improved clone_last_line()

--- static/build/js/form-table.js
+++ static/build/js/form-table.js
@@ -56,6 +56,8 @@ class Lines {
                 new_last_line.find("input").val("");
                 new_last_line.find("textarea").val("");
                 new_last_line.find("input").attr('index', last_line.find("input").attr('index')*1 + 1);
+                new_last_line.find("input").attr('id', last_line.find("input").attr('id').replace(/(-\d+)+/g, function(match, number) {
+                    return parseInt(number)-1}));
                 new_last_line.find(".index-input").val(last_line.find(".index-input").val() * 1 + 1);
                 table.append(new_last_line);
             }
