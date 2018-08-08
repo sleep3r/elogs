a218708293ede1d916af9c1c26b1dfcec8b73bc9
dimitrius <dmitry.yampolskii@inframine.io>
2018-08-08 20:27:44 +0300

    Refactored messages

--- e_logs/common/messages_app/models.py
+++ e_logs/common/messages_app/models.py
@@ -57,3 +57,21 @@ class Message(models.Model):
     class Meta:
         verbose_name = 'Сообщение'
         verbose_name_plural = 'Сообщения'
+
+    @staticmethod
+    def get_addressees(all=False, positions=None, ids=None, plant=None):
+        """Отдает список адресатов"""
+
+        res = []
+        if all:
+            return Employee.objects.only('user')
+        if positions:
+            for p in positions:
+                emp = Employee.objects.filter(plant=plant, position=p)
+                res.extend(emp)
+        if ids:
+            for id in ids:
+                emp = Employee.objects.get(id=id)
+                res.append(emp)
+
+        return res
