db2d498b1dab6369894f94a0a3ea49d2a40cd09b
Kirill Khudyakov <hudiakov@gmail.com>
2018-08-14 16:06:53 +0300

    Fix dev markup

---
+++ dev/e_logs-markup/js/popper-init.js
@@ -0,0 +1,30 @@
+
+function onEdit(item){
+  let reference = document.getElementById(item.id);
+  let popper = document.querySelector('.popper');
+  popper.style.display = "block";
+  let anotherPopper = new Popper(reference, popper, {
+      placement: 'top',
+      arrow: {
+        classNames: [ 'MY-popper-arrow' ]
+      },
+      onCreate: function(item) {
+        console.info("create popup");
+        let content = item.instance.reference.value;
+        let popup = item.instance.popper;
+        let textarea = popup.querySelector(".content");
+        console.info(textarea)
+        textarea.value = "Ваш комментарий " + content;
+      },
+      onUpdate: function() {
+        console.info("update popup");
+      }
+
+  });
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+
+});
