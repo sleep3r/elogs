bda16fbc7264096127ba88710b9e9907153afef2
“Alexander <kalashnikov@os-evolution.ru>
2018-08-28 17:34:17 +0300

    Configured celery app

--- config/settings/settings_base.py
+++ config/settings/settings_base.py
@@ -381,11 +381,3 @@ CACHEOPS = {
 #     'socket_timeout': 0.1,               # connection timeout in seconds, optional
 #     'db': 0                              # redis database, default: 0
 # }
-
-
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = TIME_ZONE
--- e_logs/common/all_journals_app/tasks.py
+++ e_logs/common/all_journals_app/tasks.py
@@ -1,6 +1,21 @@
+import os
 from celery import Celery
 
 app = Celery('tasks', broker="redis://localhost:6379")
+
+app.conf.update(
+    task_serializer='json',
+    accept_content=['json'],  # Ignore other content
+    result_serializer='json',
+    timezone='Europe/Moscow',
+)
+
+app.conf.beat_schedule = {
+    'add-every-5-seconds': {
+        'task': 'e_logs.common.all_journals_app.tasks.hello',
+        'schedule': 5.0,
+    },
+}
 
 @app.task
 def hello():
