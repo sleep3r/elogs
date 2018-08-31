ea2d4b4cf9a1783917742869e1bd933bddc713a5
“Alexander <kalashnikov@os-evolution.ru>
2018-08-31 18:58:14 +0300

    Added SettingsTemplateView

--- e_logs/common/settings_app/views.py
+++ e_logs/common/settings_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class SettingsView(TemplateView):
+    template_name = 'settings.html'
+    # model = JsonTable
+    # form = TableForms
+    # json_form = JsonForm
+
+    def post(self, request):
+        pass
+
+
+    def get_context_data(self, *args, **kwargs):
+        pass
