48b2b8bd9b080296eb37790b03c26791e2e939d6
“Alexander <kalashnikov@os-evolution.ru>
2018-08-31 17:41:46 +0300

    Revert Windows config

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -3,7 +3,16 @@ from .settings_base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db',
-    }
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'elogs',
+        'HOST': '127.0.0.1',
+        'PORT': '1433',
+        'USER': 'sa',
+        'PASSWORD': 'Singapore2017',
+
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        },
+    },
 }
+
