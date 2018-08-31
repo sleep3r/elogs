e4d0ae29d49024b791fbd46a8b4d1ac21b884c75
“Alexander <kalashnikov@os-evolution.ru>
2018-08-31 17:27:58 +0300

    Created settings_app

--- e_logs/business_logic/blank_shifts/views.py
+++ e_logs/business_logic/blank_shifts/views.py
@@ -1,15 +1,10 @@
 import json
-from datetime import timedelta
 
 from django.http import JsonResponse
-from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 
-from e_logs.business_logic.services import SetMode, SetLimitedAccess
-from e_logs.common.all_journals_app.models import Shift
-from e_logs.common.login_app.models import Employee
-from e_logs.core.models import Setting
-from e_logs.common.all_journals_app.tasks import end_of_limited_access, send_deferred_message
+from e_logs.business_logic.services import SetLimitedAccess
+
 
 @csrf_exempt
 def set_limited_access_employee_list(request):
