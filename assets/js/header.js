1cc447ff2e776281ab98ad5a4280717f2248b921
McReign <ya.manshin@mail.ru>
2018-08-31 11:01:35 +0300

    header scroll and tables layout fixed

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,11 +1,16 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  let element = document.querySelector(".header");
-  window.addEventListener('scroll', function() {
-      let scrollPos = window.pageYOffset;
-      if (scrollPos > 0) {
-            element.classList.add("sticky");
-      } else {
-            element.classList.remove("sticky");
-      }
+$(document).ready( function(event) {
+  let header = $(".header");
+  let lastScrollTop = 0;
+  $(window).scroll(function(event){
+     let st = $(this).scrollTop();
+     if(st < 100){
+         header.removeClass("hidden").addClass("sticky")
+     }
+     if((lastScrollTop - st > 2) && (st > 100)) {
+         header.removeClass("hidden").addClass("sticky");
+     } else if((st - lastScrollTop > 10) && (st > 100)) {
+         header.removeClass("sticky").addClass("hidden");
+     }
+     lastScrollTop = st;
   });
 });
