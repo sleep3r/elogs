f208635961d8627cb49cad40ed2d32e217d803cd
Kirill Khudyakov <hudiakov@gmail.com>
2018-09-03 16:14:33 +0300

    Added script `recreate database`

---
+++ scripts/recreatedb.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sqlcmd -S 127.0.0.1 -P 'Singapore2017' -U sa -Q 'DROP DATABASE elogs'
+sqlcmd -S 127.0.0.1 -P 'Singapore2017' -U sa -Q 'CREATE DATABASE elogs'
+python3 manage.py makemigrations
+python3 manage.py migrate
