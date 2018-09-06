7d530fcb06d27f308c386e21fb0c9540bbc4086f
Kirill Khudyakov <hudiakov@gmail.com>
2018-09-06 18:37:53 +0300

    Use `journal-panel`
    Change template `journal__panel.html`

--- assets/tables/index.js
+++ assets/tables/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue/dist/vue.esm.js'
 import TableCommon from './TableCommon.vue';
+import JournalPanel from './JournalPanel.vue';
 import axios from 'axios';
 
 
 Vue.component('tablecommon', TableCommon);
+Vue.component('journal-panel', JournalPanel);
+
 
 window.app = new Vue({
   el: '#elogs-app',
