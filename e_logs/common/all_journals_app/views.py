fe085dd350283053427a2346e9bf52502a61f967
“Alexander <kalashnikov@os-evolution.ru>
2018-09-06 17:03:04 +0300

    Fixed dynamic menu in in homepage

--- e_logs/common/all_journals_app/views.py
+++ e_logs/common/all_journals_app/views.py
@@ -38,7 +38,7 @@ class Index(LoginRequiredMixin, TemplateView):
             return redirect(homepage_url)
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = get_context(self.request, plant=None, journal=None)
         return context
 
 
