f3124433c7d52ebe213d08f0d5421b5db9436645
Ubuntu <ubuntu@ip-172-31-45-72.eu-central-1.compute.internal>
2018-09-10 12:29:02 +0000

    Added aws settings

--- config/settings/settings_aws.py
+++ config/settings/settings_aws.py
@@ -68,3 +68,27 @@ DATABASES = {
 }
 
 
+DEBUG = False
+
+TEMPLATES[0]['OPTIONS']['loaders'] = [  # noqa F405
+    (
+        'django.template.loaders.cached.Loader',
+        [
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        ]
+    ),
+]
+
+TEMPLATES[0]['APP_DIRS'] = False
+
+
+MIDDLEWARE.insert(1, 'django.middleware.gzip.GZipMiddleware')
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+MIDDLEWARE = [] + MIDDLEWARE + \
+             [
+                 'htmlmin.middleware.HtmlMinifyMiddleware',
+                 'htmlmin.middleware.MarkRequestMiddleware',
+             ]
+HTML_MINIFY = True
