6dc651d7773d089110c995f886d53d840c1d5987
“Alexander <kalashnikov@os-evolution.ru>
2018-09-12 16:59:05 +0300

    Redone journal_upload backend for new format

--- e_logs/common/all_journals_app/views.py
+++ e_logs/common/all_journals_app/views.py
@@ -216,10 +216,11 @@ class ConstructorView(LoginRequiredMixin, View):
         if request.FILES.get('journal_file', None):
             journal = request.FILES['journal_file']
             plant_name = self.request.POST.get('plant', None)
-            if plant_name:
+            type = self.request.POST.get('type', None)
+            if plant_name and type:
                 plant = Plant.objects.get(name=plant_name)
                 try:
-                    log = JournalBuilder(request=self.request, file=journal, plant=plant)
+                    log = JournalBuilder(request=self.request, file=journal, plant=plant, type=type)
                 except FileNotFoundError as err:
                     return render(self.request, 'settings.html',
                                   {'form_errors': str(err)})
@@ -230,13 +231,13 @@ class ConstructorView(LoginRequiredMixin, View):
 
                 return redirect('/common/settings/')
             else:
-                return render(self.request, 'settings.html', {'form_errors':'Выберите цех!'})
+                return render(self.request, 'settings.html', {'form_errors':'Выберите цех и тип!'})
 
         return render(self.request, 'settings.html', {'form_errors': 'Выберите файл журнала!'})
 
 
 class JournalBuilder():
-    def __init__(self, request, file, plant):
+    def __init__(self, request, file, plant, type):
         required_version = env('CONSTRUCTOR_VERSION')
 
         self.request = request
@@ -248,12 +249,11 @@ class JournalBuilder():
             raise FileNotFoundError('Ошибка структуры файла')
 
         if meta['version'] == float(required_version):
-            self.name = meta['name']
-            self.verbose = meta['verbose']
             self.plant = plant
-            self.type = meta['type']
-            self.tables = list(meta['tables'].keys())
-            self.tables__meta = meta['tables']
+            self.type = type
+            self.name = meta['name']
+            self.title = meta['title']
+            self.tables = meta['tables']
         else:
             raise ImportError(f"Некорректная версия, требуется не ниже v{required_version}")
 
@@ -263,22 +263,16 @@ class JournalBuilder():
 
         new_journal = self.__create_journal(tables_path)
 
-        tables_list = []
+        for table in self.tables:
+            new_table = self.__create_table(journal=new_journal, table=table)
 
-        for table_name in self.tables:
-            new_table = self.__create_table(journal=new_journal, name=table_name)
-
-            tables_list.append(f"tables/{self.plant.name}/{self.name}/{table_name}.html")
-
-            meta = self.tables__meta[table_name]['meta']
-            for field_name in meta.keys():
-                new_field = self.__create_field(table=new_table, name=field_name, meta=meta)
-
-                self.__set_field_settings(field=new_field, meta=meta)
+            for field in table['fields']:
+                new_field = self.__create_field(table=new_table, field=field)
+                self.__set_field_settings(field=new_field, meta=field)
 
         self.__extract_tables(tables_path)
 
-        Setting.of(new_journal)['tables_list'] = tables_list
+        self.__set_tables_order(journal=new_journal)
 
     def __create_journal(self, tables_path):
         journal = get_or_none(Journal, name=self.name, plant=self.plant, type=self.type)
@@ -288,44 +282,50 @@ class JournalBuilder():
                           {'form_errors': 'Журнал с таким именем уже существует!'})
         else:
             new_journal = Journal.objects.create(name=self.name,
-                                                 verbose_name=self.verbose,
+                                                 verbose_name=self.title,
                                                  plant=self.plant,
                                                  type=self.type)
             # os.makedirs(tables_path)
 
             return new_journal
 
-    def __create_table(self, name, journal):
-        table = get_or_none(Table, name=name, journal=journal)
+    def __create_table(self, journal, table):
+        table = get_or_none(Table, name=table['name'], journal=journal)
 
         if table:
             journal.delete()
             return render(self.request, 'settings.html',
-                          {'form_errors': f'Две таблицы с одинаковым именем {name}!'})
+                          {'form_errors': f'Две таблицы с одинаковым именем {table["name"]}!'})
         else:
-            new_table = Table.objects.create(name=name,
+            new_table = Table.objects.create(name=table['name'],
                                         journal=journal,
-                                        verbose_name=self.tables__meta[name].get('verbose', None))
+                                        verbose_name=table.get('title', None))
             return new_table
 
-    def __create_field(self, table, name, meta):
-        field = get_or_none(Field, name=name, table=table)
+    def __create_field(self, table, field):
+        field = get_or_none(Field, name=field['name'], table=table)
 
         if field:
             table.journal.delete()
             return render(self.request, 'settings.html',
-                          {'form_errors': f'Две столбца с одинаковым именем {name}!'})
+                          {'form_errors': f'Две столбца с одинаковым именем {field["name"]}!'})
         else:
-            new_field = Field.objects.create(name=name,
+            new_field = Field.objects.create(name=field.pop('name'),
                                              table=table,
-                                             verbose_name=meta[name].get('verbose', None))
+                                             verbose_name=field.get('title', None))
             return new_field
 
     def __set_field_settings(self, field, meta):
-        Setting.of(field)["field_description"] = meta[field.name]
+        Setting.of(field)["field_description"] = meta
 
     def __extract_tables(self, tables_path):
         for table in self.file.infolist():
             if table.filename.endswith('.html'):
                 table.filename = os.path.basename(table.filename)
                 self.file.extract(table, tables_path)
+
+    def __set_tables_order(self, journal):
+        tables_list = []
+        for table in sorted(self.tables, key=lambda t:t['order']):
+            tables_list.append(f"tables/{self.plant.name}/{self.name}/{table.name}.html")
+        Setting.of(journal)['tables_list'] = tables_list
