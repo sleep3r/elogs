a2769d5b0ab4409d6bd6078fd281654212905e8f
McReign <ya.manshin@mail.ru>
2018-09-20 15:55:42 +0300

    Editing mode added

--- assets/js/modes-menu.js
+++ assets/js/modes-menu.js
@@ -12,30 +12,43 @@ function setData() {
     setListeners()
 }
 
+function setModeData() {
+    $('#modeTitle').text(currentMode.message)
+    currentMode.fields.map(item =>
+        $('.mode-content__body tbody').append(`<tr>
+                                                    <td data-target="table_name">${item.table_name}</td>
+                                                    <td data-target="name">${item.name}</td>
+                                                    <td data-target="min_normal">${item.min_normal}</td>
+                                                    <td data-target="max_normal">${item.max_normal}</td>
+                                               </tr>`)
+    )
+    $('.mode-content__title__mode-btns').css({display: 'block'})
+    let $onOfBtn = $('button[data-target="mode-on-off"]')
+    $onOfBtn.text(currentMode.is_active ? 'Выключить' : 'Включить')
+    $onOfBtn.val('data-value', currentMode.is_active ? 'on' : 'off')
+    $('.mode-content__body').css({display: 'block'})
+}
+
+function removeModeData() {
+    $('.menu--modes .menu__item').removeClass('selected')
+    $('.mode-content__body tbody').html('')
+    $('#modeTitle').text('')
+    $('.mode-content__body').css({display: 'none'})
+    $('.mode-content__title__mode-btns').css({display: 'none'})
+    $('.mode-content__title__mode-edit-btns').css({display: 'none'})
+    $('button[data-target="mode-on-off"]').val('data-value', 'off')
+}
+
 function setListeners() {
     $('.menu--modes .menu__item').click(function () {
-        $('.menu--modes .menu__item').removeClass('selected')
+        removeModeData()
         $(this).addClass('selected')
-
-        $('.mode-content__body tbody').html('')
         currentMode = getCurrentMode(+$(this).attr('id'))
-
-        $('#modeTitle').text(currentMode.message)
-        currentMode.fields.map(item =>
-            $('.mode-content__body tbody').append(`<tr>
-                                                        <td data-target="table_name">${item.table_name}</td>
-                                                        <td data-target="name">${item.name}</td>
-                                                        <td data-target="min_normal">${item.min_normal}</td>
-                                                        <td data-target="max_normal">${item.max_normal}</td>
-                                                   </tr>`)
-        )
-        $('.mode-content__title__mode-btns').css({display: 'block'})
-        $('button[data-target="mode-on-off"]').text(currentMode.is_active ? 'Выключить' : 'Включить')
-        $('.mode-content__body').css({display: 'block'})
+        setModeData()
     })
 
     $('button[data-target="mode-on-off"]').click(function () {
-
+        toggleModeActive($('button[data-target="mode-on-off"]').val('data-value') === 'on' ? 0 : 1)
     })
 
     $('button[data-target="mode-edit"]').click(function () {
@@ -61,8 +74,8 @@ function setListeners() {
         $('.mode-content__title__mode-edit-btns').css({display: 'none'})
         $('.mode-content__title__mode-btns').css({display: 'block'})
 
-        updateFieldItems()
-        console.log(currentMode)
+        let updatedMode = Object.assign({}, currentMode, { fields: updateFieldItems() })
+        updateMode(updatedMode)
     })
 
     $('button[data-target="mode-add-row"]').click(function () {
@@ -90,25 +103,25 @@ function getModes() {
         });
 }
 
-function updateFieldItems() {
-    // $('.mode-content__body tbody tr').each(function () {
-    //     let currentField = currentMode.fields.filter(item => item.name === $(this).children('td[data-target="name"]').text())[0]
-    //     if (!currentField) {
-    //         currentField = {
-    //             name: $(this).children('td[data-target="name"]').text()
-    //         }
-    //     }
-    //     currentField.table_name = $(this).children('td[data-target="table_name"]').text()
-    //     currentField.name = $(this).children('td[data-target="name"]').text()
-    //     currentField.min_normal = $(this).children('td[data-target="min_normal"]').text()
-    //     currentField.max_normal = $(this).children('td[data-target="max_normal"]').text()
-    //
-    //     currentMode.fields.map(item => item.name === currentField.name ? item = currentField : currentMode.fields.push(currentField))
-    // })
+function getCurrentMode(id) {
+    return modes.filter(item => item.id === id)[0]
 }
 
-function updateMode() {
-    return axios.put('/bl/modes_api/', currentMode)
+function updateFieldItems() {
+    let updatedFields = []
+    $('.mode-content__body tbody tr').each(function () {
+        updatedFields.push({
+            table_name: $(this).children('td[data-target="table_name"]').text(),
+            name: $(this).children('td[data-target="name"]').text(),
+            min_normal: +$(this).children('td[data-target="min_normal"]').text(),
+            max_normal: +$(this).children('td[data-target="max_normal"]').text()
+        })
+    })
+    return updatedFields
+}
+
+function updateMode(mode) {
+    return axios.put('/bl/modes_api/', mode)
         .then(response => {
             location.reload()
         })
@@ -117,6 +130,22 @@ function updateMode() {
         });
 }
 
-function getCurrentMode(id) {
-    return modes.filter(item => item.id === id)[0]
+function toggleModeActive(isActive) {
+    return axios.put('/bl/modes_api/', { id: currentMode.id, is_active: isActive })
+        .then(response => {
+            location.reload()
+        })
+        .catch(e => {
+            console.log(e)
+        });
+}
+
+function deleteMode(mode) {
+    return axios.delete('/bl/modes_api/')
+        .then(response => {
+            location.reload()
+        })
+        .catch(e => {
+            console.log(e)
+        });
 }
