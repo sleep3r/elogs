96ade0db991a2b94ce094061f92bc904335d0d1b
dimitrius <dmitry.yampolskii@inframine.io>
2018-09-24 23:06:40 +0300

    Updated everything

--- config/asgi.py
+++ config/asgi.py
@@ -1,4 +1,15 @@
 from config import settings_setup
 from channels.routing import get_default_application
+import os
+import time
+import traceback
+import signal
+import sys
 
-application = get_default_application()
+try:
+    application = get_default_application()
+except Exception:
+    print('handling WSGI exception')
+    traceback.print_exc()
+    os.kill(os.getpid(), signal.SIGINT)
+    time.sleep(2.5)
