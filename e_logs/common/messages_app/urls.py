66d233b1d2297ade050d35a056835f58e0218724
“Alexander <kalashnikov@os-evolution.ru>
2018-10-02 16:41:12 +0300

    Added web push sub url

--- e_logs/common/messages_app/urls.py
+++ e_logs/common/messages_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from e_logs.common.messages_app import views
+from e_logs.common.messages_app.views import MessagesSubscription
 from e_logs.core.utils.webutils import process_json_view
 
 urlpatterns = [
     path('get/', views.msg_view, name='messages_get'),
     path('read/', views.msg_view, name='messages_read'),
     path('list/', views.MessagesList.as_view(), name='messages_list'),
+    path('subscribe/', MessagesSubscription.as_view(), name='messages_sub'),
 ]
