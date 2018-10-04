fad900713cb44d5bfd635c02fb1e2ad603c5a33f
McReign <ya.manshin@mail.ru>
2018-10-04 16:36:58 +0300

    Web notifications repaired

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,12 +1,20 @@
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
+const path = require('path');
+
 module.exports = {
-  configureWebpack: {
-    module: {
-    	rules: [
-    		{
-          test: /\.pdf$/,
-          loader: 'file-loader',
-        }
-      ]
+    configureWebpack: {
+        module: {
+            rules: [
+                {
+                    test: /\.pdf$/,
+                    loader: 'file-loader',
+                }
+            ]
+        },
+        plugins: [
+            new ServiceWorkerWebpackPlugin({
+                entry: path.join(__dirname, 'src/sw2.js'),
+            }),
+        ]
     }
-  }
 }
