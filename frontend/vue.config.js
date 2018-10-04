41883dc19e753721afe50739096a6802c0270d45
“Alexander <kalashnikov@os-evolution.ru>
2018-10-04 12:23:55 +0300

    Returned http frontend server

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,25 +1,12 @@
-const fs = require('fs')
-
 module.exports = {
-    devServer: {
-        open: process.platform === 'darwin',
-        host: '127.0.0.1',
-        port: 8080,
-        https: {
-            key: fs.readFileSync('../server.key'), // путь до твоего файла
-            cert: fs.readFileSync('../server.crt'), // путь до твоего файла
-            ca: fs.readFileSync('../Local Certificate.pem'), // путь до твоего файла
-        },
-        hotOnly: false,
-    },
-    configureWebpack: {
-        module: {
-            rules: [
-                {
-              test: /\.pdf$/,
-              loader: 'file-loader',
-            }
-          ]
+  configureWebpack: {
+    module: {
+    	rules: [
+    		{
+          test: /\.pdf$/,
+          loader: 'file-loader',
         }
+      ]
     }
+  }
 }
