d71c4750019972b6c6e17e6a625df4bd2527572d
“Alexander <kalashnikov@os-evolution.ru>
2018-10-05 18:30:14 +0300

    Messages api

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from django.conf.urls import url, include
+from django.conf.urls import include
 from django.urls import path, re_path
-from django.contrib.auth.decorators import user_passes_test
 
 from rest_framework_swagger.views import get_swagger_view
 
 from django.conf import settings
-from e_logs.common.all_journals_app.views import JournalView, get_shifts, Index, get_table_template, get_menu_info
+from e_logs.common.all_journals_app.views import JournalView, get_shifts, Index, get_table_template
 
 handler403 = "e_logs.common.all_journals_app.views.permission_denied"
 schema_view = get_swagger_view(title='E-LOGS API')
@@ -25,11 +24,10 @@ urlpatterns = [
     re_path(r'^api/auth/', include('djoser.urls.base')),
     re_path(r'^api/auth/', include('djoser.urls.authtoken')),
     re_path(r'^api/auth/', include('djoser.urls.jwt')),
-    re_path(r'^api/docs/$', user_passes_test(lambda u: u.is_superuser)(schema_view)),
     re_path(r'^api/', include('e_logs.common.all_journals_app.api.urls')),
+    re_path(r'^api/', include('e_logs.common.messages_app.api.urls')),
     path('bl/', include('e_logs.business_logic.modes.urls')),
     path('bl/', include('e_logs.business_logic.blank_shifts.urls')),
-    path('furnace/fractional/', include('e_logs.furnace.fractional_app.urls')),
     path('templates/tables/<str:plant_name>/<str:journal_name>/<str:table_name>/', get_table_template),
     path('<str:plant_name>/<str:journal_name>/', JournalView.as_view()),
     path('<str:plant_name>/<str:journal_name>/<int:page_id>/', JournalView.as_view(),
