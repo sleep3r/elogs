3be5aefda54350761a6fcf7cd6469e59eeea7b27
dimitrius <dmitry.yampolskii@inframine.io>
2018-10-23 17:12:35 +0600

    Deleted unneeded files

--- e_logs/core/apps.py
+++ e_logs/core/apps.py
@@ -5,8 +5,8 @@ class CoreConfig(AppConfig):
     name = 'e_logs.core'
     verbose_name = 'Настройки'
 
-
 from suit.apps import DjangoSuitConfig
+
 
 class SuitConfig(DjangoSuitConfig):
     layout = 'vertical'
