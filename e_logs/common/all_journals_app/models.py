cb8fdab6a992646fdd8c543c6512ce688f3ece21
“Alexander <kalashnikov@os-evolution.ru>
2018-11-12 21:29:27 +0300

    Added int() checks for number of shifts

--- e_logs/common/all_journals_app/models.py
+++ e_logs/common/all_journals_app/models.py
@@ -161,14 +161,14 @@ class Shift(CellGroup):
     @property
     def start_time(self) -> timezone.datetime:
         number_of_shifts = Shift.get_number_of_shifts(self.journal)
-        shift_hour = (8 + (self.order - 1) * (24 // number_of_shifts)) % 24
+        shift_hour = (8 + (self.order - 1) * (24 // int(number_of_shifts))) % 24
         shift_time = time(hour=shift_hour, tzinfo=timezone.get_current_timezone())
         return datetime.combine(self.date, shift_time)
 
     @property
     def end_time(self) -> timezone.datetime:
         number_of_shifts = Shift.get_number_of_shifts(self.journal)
-        shift_length = timedelta(hours=24 // number_of_shifts)
+        shift_length = timedelta(hours=24 // int(number_of_shifts))
         return self.start_time + shift_length
 
     def is_active(self, time=timezone.now()) -> bool:
