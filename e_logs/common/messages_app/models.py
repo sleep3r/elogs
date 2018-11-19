8ebd6f9c86fb35aa6cba6fe9967cdb01b99f45bd
Alexander Kalashnikov <sleep3r@icloud.com>
2018-11-19 14:27:39 +0300

    Disabled push notify

--- e_logs/common/messages_app/models.py
+++ e_logs/common/messages_app/models.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 from pywebpush import webpush, WebPushException
 
 from e_logs.common.login_app.models import Employee
-from e_logs.core.utils.webutils import filter_or_none, StrAsDictMixin, get_or_none
+from e_logs.core.utils.webutils import filter_or_none, StrAsDictMixin, get_or_none, user_to_response
 
 
 class Message(StrAsDictMixin, models.Model):
@@ -93,41 +93,41 @@ class Message(StrAsDictMixin, models.Model):
                           {
                             'type': 'messages',
                             'cell': cell.field.name if cell else None,
-                            'sendee': {str(message['sendee'].user):message['sendee'].name} if message['sendee'] else 'E-LOGS',
+                            'sendee': user_to_response(message['sendee']),
                             'text': text}
                       )})
 
-                Message.push_notification(title='E-LOGS', body='Новое сообщение', user_id=emp.id)
+                # Message.push_notification(title='E-LOGS', body='Новое сообщение', user_id=emp.id)
 
-    @staticmethod
-    def push_notification(title, body, user_id):
-        user_subscriptions = UserSubscription.objects.filter(user_id=user_id)
-        for subscription in user_subscriptions:
-            data = json.dumps({
-                'title': title,
-                'body': body,
-            })
-            try:
-                print(subscription.subscription)
-                webpush(
-                    subscription_info=json.loads(subscription.subscription),
-                    data=data,
-                    vapid_private_key='./private_key.pem',
-                    vapid_claims={
-                        'sub': 'mailto:inframine@inframine.io',
-                    }
-                )
-            except WebPushException as ex:
-                print('I can\'t do that: {}'.format(repr(ex)))
-                print(ex)
-                # Mozilla returns additional information in the body of the response.
-                if ex.response and ex.response.json():
-                    extra = ex.response.json()
-                    print('Remote service replied with a {}:{}, {}',
-                          extra.code,
-                          extra.errno,
-                          extra.message
-                          )
+    # @staticmethod
+    # def push_notification(title, body, user_id):
+    #     user_subscriptions = UserSubscription.objects.filter(user_id=user_id)
+    #     for subscription in user_subscriptions:
+    #         data = json.dumps({
+    #             'title': title,
+    #             'body': body,
+    #         })
+    #         try:
+    #             print(subscription.subscription)
+    #             webpush(
+    #                 subscription_info=json.loads(subscription.subscription),
+    #                 data=data,
+    #                 vapid_private_key='./private_key.pem',
+    #                 vapid_claims={
+    #                     'sub': 'mailto:inframine@inframine.io',
+    #                 }
+    #             )
+    #         except WebPushException as ex:
+    #             print('I can\'t do that: {}'.format(repr(ex)))
+    #             print(ex)
+    #             # Mozilla returns additional information in the body of the response.
+    #             if ex.response and ex.response.json():
+    #                 extra = ex.response.json()
+    #                 print('Remote service replied with a {}:{}, {}',
+    #                       extra.code,
+    #                       extra.errno,
+    #                       extra.message
+    #                       )
     
     @staticmethod
     def update(cell):
