2c0ec644ce33ff4bea72061c3fef11a5944c319c
Alexander Kalashnikov <sleep3r@icloud.com>
2018-12-11 01:29:24 +0300

    codestyle

--- e_logs/core/views.py
+++ e_logs/core/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.mixins import AccessMixin
 from django.http import HttpResponseForbidden
 
+
 class LoginRequired(AccessMixin):
     """Verify that the current user is authenticated."""
+
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return HttpResponseForbidden()
