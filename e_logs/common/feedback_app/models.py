8ae02db4c085ce96e5cabed0a6f20eef5a5b1b10
nikitakrutoy <nikitakrutoy@gmail.com>
2018-12-12 22:53:40 +0300

    updated email model $ added email files support

--- e_logs/common/feedback_app/models.py
+++ e_logs/common/feedback_app/models.py
@@ -6,9 +6,7 @@ from django.conf import settings
 from e_logs.core.utils.webutils import StrAsDictMixin
 from e_logs.core.utils.loggers import err_logger
 from .services.telegram_bot import TelegramBot
-from email.mime.text import MIMEText
-# from email.mime.multipart import MIMEMultipart
-from email.header import Header
+from .services.email import Email, EmailConnection
 
 import os
 import smtplib
@@ -27,21 +25,11 @@ class Feedback(StrAsDictMixin, models.Model):
         """
     )
 
-    # MAIL = textwrap.dedent(
-    #     """
-    #     From: {email}
-    #     To: {email}
-    #     Subject: "Kazzinc Elog Feedback"
-
-    #     {msg}
-    #     """
-    # )
-
-
 
     bot = TelegramBot(channel=settings.FEEDBACK_TG_BOT["channel"],
                       token=settings.FEEDBACK_TG_BOT["token"],
                       proxy=settings.FEEDBACK_TG_BOT["proxy"])
+
 
     theme = models.CharField(max_length=200, verbose_name='Тема', null=True, blank=True)
     text = models.CharField(max_length=1000, verbose_name='Сообщение', null=True, blank=True)
@@ -51,12 +39,9 @@ class Feedback(StrAsDictMixin, models.Model):
     filenames = models.TextField(verbose_name="Имена файлов", default="")
     username = models.CharField(max_length=200, blank=True, null=True, verbose_name='Пользователь')
 
-    class Meta:
-        verbose_name = 'Отзыв'
-        verbose_name_plural = 'Отзывы'
-
-    def send_feedback(self):
-        msg = Feedback.MESSAGE.format(
+    @property
+    def message(self):
+        return Feedback.MESSAGE.format(
             usr=self.username,
             email=self.email,
             plant=self.plant,
@@ -64,41 +49,36 @@ class Feedback(StrAsDictMixin, models.Model):
             theme=self.theme,
             text=self.text,
         )
-        # self._send_telegram(msg)
-        self._send_mail(msg)
 
 
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
+    def send_feedback(self):
+        self._send_telegram()
+        self._send_mail()
 
 
-    def _send_telegram(self, msg):
-        Feedback.bot.send_message(msg)
-        filenames = self.filenames.split(",")
-        if filenames[0]:
-            files = []
-            for filename in filenames:
-                dirpath = os.path.join(os.path.dirname(__file__), "media")
-                filepath = os.path.join(dirpath, filename)
-                files.append(open(filepath, "rb"))
-            Feedback.bot.send_media(files)
+    def _send_telegram(self):
+        Feedback.bot.send_message(self.message)
+        if self.filenames:
+            Feedback.bot.send_media(self.filenames.split(','))
         return self
     
-    def _send_mail(self, msg):
-        msg = msg.replace('\n', '<br/>')
-        print(msg)
-        mail = settings.FEEDBACK_MAIL["mail"]
-        password = settings.FEEDBACK_MAIL["password"]
+    def _send_mail(self):
+        message = self.message.replace('\n', '<br/>')
         to_mail = settings.FEEDBACK_MAIL["to"]
-        msg = MIMEText(msg, "html", _charset="UTF-8")
-        msg['Subject'] = Header("Kazzinc Elog Feedback", "utf-8")
-        # mail_text = Feedback.MAIL.format(
-        #     email=mail,
-        #     msg=msg
-        # )
-        try:  
-            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-            server.ehlo()
-            server.login(mail, password)
-            server.sendmail(mail, to_mail, msg.as_string())
-            server.close()
+        from_mail = settings.FEEDBACK_MAIL["mail"]
+        password = settings.FEEDBACK_MAIL["password"]
+        subject = "Kazzinc Elog Feedback"
+        print(self.filenames)
+        email = Email(from_mail, to_mail, subject, message, 
+            message_type='html', attachments=self.filenames.split(','),
+            message_encoding="utf-8",
+        )
+        conn = EmailConnection('smtp.gmail.com:465', to_mail, password)
+        try:
+            conn.send(email)
         except Exception as e:
             err_logger.critical("Почта накрылась!", e)
