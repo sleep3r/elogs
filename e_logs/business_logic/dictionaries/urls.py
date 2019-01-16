77c223c6776dd208932fd570cb9b0a8cd4896159
Alexander Kalashnikov <sleep3r@icloud.com>
2019-01-16 21:50:52 +0300

    Changed dict names and api structure

--- e_logs/business_logic/dictionaries/urls.py
+++ e_logs/business_logic/dictionaries/urls.py
@@ -5,11 +5,11 @@ from .views import *
 
 urlpatterns = [
     path('get_dicts/', DictionariesApi.as_view()),
-    path('autocomplete/', AutocompleteAPI.as_view(), name="Фамилия И.О."),
-    path('usernames/', UsernamesAPI.as_view(), name="Имена пользователей"),
-    path('concentrates/', ConcentratesAPI.as_view(), name="Концентраты"),
-    path('equipment/', EquipmentAPI.as_view(), name="Оборудование"),
-    path('emails/', EmailsAPI.as_view(), name="Электронные почты"),
+    path('autocomplete/', AutocompleteAPI.as_view(), name="Список фамилий"),
+    path('usernames/', UsernamesAPI.as_view(), name="Список пользователей"),
+    path('concentrates/', ConcentratesAPI.as_view(), name="Список концентратов"),
+    path('equipment/', EquipmentAPI.as_view(), name="Список оборудования"),
+    path('emails/', EmailsAPI.as_view(), name="Список электронных почт"),
 
 
 ]
